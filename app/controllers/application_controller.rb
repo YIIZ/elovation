5f8d055cfd3aa77c56ef82c599c60d6adeba8f14
Bin Xin <rhyzix@gmail.com>
2018-12-06 00:04:25 +0800

    admin auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  http_basic_authenticate_with unless: -> { ENV['ADMIN_BASIC_AUTH_PASSWORD'].blank? || request.get? },
+    name: ENV['ADMIN_BASIC_AUTH_NAME'],
+    password: ENV['ADMIN_BASIC_AUTH_PASSWORD']
+
+  # def admin_authenticate
+  # end
 end

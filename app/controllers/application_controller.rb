396fce6cbb35a3f6d2b4cb50e6b4255c2cb3c5fd
Bin Xin <rhyzix@gmail.com>
2018-12-06 16:24:14 +0800

    auth new and edit action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  http_basic_authenticate_with unless: -> { ENV['ADMIN_BASIC_AUTH_PASSWORD'].blank? || request.get? },
+  http_basic_authenticate_with unless: ->(c) {
+      # TODO by rails config Rails.configuration.authentication['name']
+      ENV['ADMIN_BASIC_AUTH_PASSWORD'].blank? ||
+      (request.get? && c.action_name != 'new' && c.action_name != 'edit')
+    },
     name: ENV['ADMIN_BASIC_AUTH_NAME'],
     password: ENV['ADMIN_BASIC_AUTH_PASSWORD']
 
